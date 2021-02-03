19cd39f6702410bde30746773612ed3527559fb9
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2021-02-03 12:59:55 +0900

    回復アイテムを取った時に音が流れるようになりました。

--- sotukenGame/Sample_15_05/Sample_15_05/RecoveryItem.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/RecoveryItem.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "RecoveryItem.h"
 #include "Player.h"
+#include "sound/SoundSource.h"
 
 const float RECOVERY_HP = 100.0f;		//HPの回復量。
 
@@ -41,6 +42,10 @@ void RecoveryItem::RecoveryPlayerHP()
 		CharacterController& charaCon = *player->GetCharacterController();
 		g_physics.ContactTestCharaCon(charaCon, [&](const btCollisionObject& collisionObject) {
 			if (m_ghostObject.IsSelf(collisionObject) == true) {
+				//SEを再生する。
+				CSoundSource* SE_Recovery = NewGO<CSoundSource>(0);
+				SE_Recovery->Init(L"Assets/sound/SE_Recovery.wav");
+				SE_Recovery->Play(false);
 				//当たったらプレイヤーのHPを回復する。
 				player->SetHP(RECOVERY_HP);
 				player->SetBeforeHp(player->GetHP());
